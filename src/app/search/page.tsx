6faff5e8f80e79abe00f88fbcd549ce92664809b
Steinar Bragi Sigurðarson <sbsig@me.com>
2025-02-12 12:50:53 +0000

    search spacing mobile

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -108,7 +108,7 @@ export default function SearchPage() {
             </p>
           </div>
 
-          <div className='mb-8 flex gap-4'>
+          <div className='mb-8 flex flex-col gap-4 sm:flex-row'>
             <input
               type='text'
               value={query}
