692fd0e73fca7b8df22fdac2d102dec1cbb729c5
Steinar Bragi Sigurðarson <sbsig@me.com>
2025-02-12 12:47:25 +0000

    front page padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className='min-h-screen bg-gradient-to-b from-blue-100 via-purple-100 to-pink-100 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20'>
+    <div className='min-h-screen bg-gradient-to-b from-blue-100 via-purple-100 to-pink-100 px-3 py-20 font-[family-name:var(--font-geist-sans)] sm:p-20'>
       <header className='text-center'>
         <h1
           className='mb-2 bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-5xl font-bold text-transparent'
